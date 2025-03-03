73255ece861f9ce4d29b5c4314e675980b54f247
Gino2525 <jinomathew42@gmail.com>
2025-03-04 00:09:39 +0530

    committed

--- LoanMAnagementSystem/urls.py
+++ LoanMAnagementSystem/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include 
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"message": "Loan Management API is Live!"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('LBE.urls')),
+      path('', home),
 ]
 
